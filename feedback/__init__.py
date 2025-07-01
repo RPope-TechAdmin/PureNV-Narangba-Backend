1d72de4ad5ea768571bb0dd3d1c6cc708e272cdd
RPope-TechAdmin <rpope@purenv.au>
2025-07-02 08:35:51 +1000

    Reorganising audience client code

--- feedback/__init__.py
+++ feedback/__init__.py
@@ -27,7 +27,7 @@ def validate_token(token):
         token,
         signing_key.key,
         algorithms=["RS256"],
-        audience="api://87cbd10b-1303-4056-a899-27bd61691211",
+        audience=f"api://{client_id}",
         issuer="https://sts.windows.net/655e497b-f0e8-44ed-98fb-77680dd02944/"
     )
     return decoded
