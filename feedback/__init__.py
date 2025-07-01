3295655fae377dc8b51bf1693d744da289f823f1
RPope-TechAdmin <rpope@purenv.au>
2025-07-01 12:20:30 +1000

    Adding slash

--- feedback/__init__.py
+++ feedback/__init__.py
@@ -24,7 +24,7 @@ def validate_token(token):
         signing_key.key,
         algorithms=["RS256"],
         audience=f"api://{client_id}",
-        issuer="https://sts.windows.net/655e497b-f0e8-44ed-98fb-77680dd02944"
+        issuer="https://sts.windows.net/655e497b-f0e8-44ed-98fb-77680dd02944/"
     )
     return decoded
 
