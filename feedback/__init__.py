3b6fd56310d81fef0fd25c9777c8688dfb800876
RPope-TechAdmin <rpope@purenv.au>
2025-07-03 12:11:55 +1000

    adding proper engine referencing with attempted correct encryption

--- feedback/__init__.py
+++ feedback/__init__.py
@@ -37,19 +37,12 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
         )
 
     try:
-        # 🔌 SQLAlchemy + pytds connection
-        def get_db_engine():
-            username = os.environ["SQL_USER"]
-            password = os.environ["SQL_PASSWORD"]
-            server = os.environ["SQL_SERVER"]
-            db = os.environ["SQL_DB"]
 
-            engine = create_engine(
-                f"mssql+pytds://{username}:{password}@{server}/{db}?encrypt=True",
-                connect_args={"autocommit": True}
-            )
-            return engine
 
+        engine = create_engine(
+            f"mssql+pytds://{os.environ['SQL_USER']}:{os.environ['SQL_PASSWORD']}@{os.environ['SQL_SERVER']}/{os.environ['SQL_DB']}?encrypt=True",
+            connect_args={"autocommit": True}
+        )
         with engine.connect() as conn:
             conn.execute(
                 text("INSERT INTO Narangba.Feedback (Name, Feedback) VALUES (:name, :feedback)"),
