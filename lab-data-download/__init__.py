c2c4e1f324773a91244bb2c192293e30ccdfcbf8
RPope-TechAdmin <rpope@purenv.au>
2025-10-31 11:09:43 +1000

    Modifying specific parameters for Narangba specific tables

--- lab-data-download/__init__.py
+++ lab-data-download/__init__.py
@@ -17,45 +17,32 @@ SQL_PASSWORD = "Rep0r7D47aD0wn"
 
 # Map HTML group ids → fully-qualified table names (include schema!)
 GROUP_TO_TABLE: Dict[str, str] = {
-    "fixtotal": "Wacol.Fixation2025FullScan",     # Fixation Totals
-    "fixtclp": "Wacol.Fixation2025TCLP",    # Fixation TCLP Values
-    "twint": "Wacol.TradeWaste2025",     # Internal Trade Waste Testing
+    "fixtotal": "Narangba.Fixation",     # Fixation Totals
+    "fixtclp": "Narangba.FixationTCLP",    # Fixation TCLP Values
+    "twint": "Narangba.TradeWaste",     # Internal Trade Waste Testing
     "twext": "Wacol.TradeWasteExt",     # Urban Utilities Trade Waste Testing
     }
 
 # Whitelist analyte columns per table to avoid SQL injection via column names.
 # ⚠️ 
 ALLOWED_COLUMNS: Dict[str, Iterable[str]] = {
-    "Wacol.Fixation2025FullScan": {
-        "Moisture Content","Antimony","Arsenic","Barium","Beryllium","Boron","Cadmium","Chromium","Cobalt","Copper","Lead","Manganese","Molybdenum","Nickel","Selenium","Vanadium","Tin","Zinc","Mercury","Initial pH","After HCl pH","Extraction Fluid Number","Final pH","Extraction Fluid Number ZHE","alpha-BHC","Hexachlorobenzene (HCB)"
-        ,"beta-BHC","gamma-BHC - (Lindane)","delta-BHC","Heptachlor","Aldrin","Heptachlor epoxide","Total Chlordane (sum)","trans-Chlordane","alpha-Endosulfan","cis-Chlordane","Dieldrin","4.4`-DDE","Endrin","Endosulfan (sum)","beta-Endosulfan","4.4`-DDD","Endrin aldehyde","Endosulfan sulfate","4.4`-DDT","Endrin ketone","Methoxychlor"
-        ,"Sum of DDD + DDE + DDT","Sum of Aldrin + Dieldrin","Dichlorvos","Demeton-S-methyl","Monocrotophos","Dimethoate","Diazinon","Chlorpyrifos-methyl","Parathion-methyl","Malathion","Fenthion","Chlorpyrifos","Parathion","Pirimphos-ethyl","Chlorfenvinphos","Bromophos-ethyl","Fenamiphos","Prothiofos","Ethion","Carbophenothion","Azinphos Methyl"
-        ,"Phenol","2-Chlorophenol","2-Methylphenol","3- & 4-Methylphenol","2-Nitrophenol","2.4-Dimethylphenol","2.4-Dichlorophenol","2.6-Dichlorophenol","4-Chloro-3-methylphenol","2.4.6-Trichlorophenol","2.4.5-Trichlorophenol","Pentachlorophenol","BenzeneMAH","TolueneMAH","EthylbenzeneMAH","meta- & para-XyleneMAH","StyreneMAH","ortho-XyleneMAH"
-        ,"IsopropylbenzeneMAH","n-PropylbenzeneMAH","1.3.5-TrimethylbenzeneMAH","sec-ButylbenzeneMAH","1.2.4-TrimethylbenzeneMAH","tert-ButylbenzeneMAH","p-IsopropyltolueneMAH","n-ButylbenzeneMAH","Naphthalene","Acenaphthylene","Acenaphthene","Fluorene","Phenanthrene","Anthracene","Fluoranthene","Pyrene","Benz(a)anthracene","Chrysene","Benzo(b+j)fluoranthene"
-        ,"Benzo(k)fluoranthene","Benzo(a)pyrene","Indeno(1.2.3.cd)pyrene","Dibenz(a.h)anthracene","Benzo(g.h.i)perylene","Sum of polycyclic aromatic hydrocarbons","Benzo(a)pyrene TEQ (zero)","Benzo(a)pyrene TEQ (half LOR)","Benzo(a)pyrene TEQ (LOR)","C6 - C9 Fraction","C10 - C14 Fraction","C15 - C28 Fraction","C29 - C36 Fraction","C10 - C36 Fraction (sum)"
-        ,"C6 - C10 Fraction","C6 - C10 Fraction  minus BTEX (F1)",">C10 - C16 Fraction",">C16 - C34 Fraction",">C34 - C40 Fraction",">C10 - C40 Fraction (sum)",">C10 - C16 Fraction minus Naphthalene (F2)","Benzene","Toluene","Ethylbenzene","meta- & para-Xylene","ortho-Xylene","Total Xylenes","Sum of BTEX","BTEXNaphthalene","Dibromo-DDE","DEF","Phenol-d6"
-        ,"2-Chlorophenol-D4","2.4.6-Tribromophenol","2-Fluorobiphenyl","Anthracene-d10","4-Terphenyl-d14","1.2-Dichloroethane-D4","Toluene-D8","4-Bromofluorobenzene"
-        
+    "Narangba.Fixation": {
+        "pH","Moisture Content","Arsenic","Cadmium","Chromium","Copper","Lead","Nickel","Zinc","Mercury","Initial pH","After HCl pH","Extraction Fluid Number","Final pH","TCLP Extraction Fluid Number","Naphthalene","Acenaphthalene"
+        ,"Acenaphthene","Fluorene","Phenathrene","Anthracene","Fluoranthene","Pyrene","Benz-a-anthracene","Chrysene","Benzo-b+j-fluoranthene","Benzo-k-fluoranthene","Benzo-a-pyrene","Indeno-1,2,3,cd-pyrene","Dibenz-a,h-anthracene","Benzo-g,h,i-Perylene"
+        ,"PAH's Sum","Benzo-a-pyrene TEQ 0","Benzo-a-pyrene TEQ Half LOR","Benzo-a-pyrene TEQ LOR","C6 - C9 Fraction","C10 - C14 Fraction","C15 - C28 Fraction","C29 - C36 Fraction","C10 - C36 Fraction (sum)","C6 - C10 Fraction","C6 - C10 Fraction  minus BTEX (F1)"
+        ,">C10 - C16 Fraction",">C16 - C34 Fraction",">C34 - C40 Fraction",">C10 - C40 Fraction (sum)",">C10 - C16 Fraction minus Naphthalene (F2)","Benzene","Toluene","Ethylbenzene","meta- & para-Xylene","ortho-Xylene","Total Xylenes","Sum of BTEX","BTEXNaphthalene"
+        ,"1.2-Dichloroethane-D4","Toluene-D8","4-Bromofluorobenzene"
     },
-    "Wacol.Fixation2025TCLP": {
-        "Antimony","Arsenic","Barium","Beryllium","Boron","Cadmium","Chromium","Cobalt","Copper","Lead","Manganese","Nickel","Selenium","Vanadium","Tin","Zinc","Molybdenum","Mercury","alpha-BHC","Hexachlorobenzene (HCB)","beta-BHC","gamma-BHC - (Lindane)","delta-BHC","Heptachlor","Aldrin","Heptachlor epoxide","trans-Chlordane","alpha-Endosulfan","cis-Chlordane",
-        "Dieldrin","4.4`-DDE","Endrin","beta-Endosulfan","4.4`-DDD","Endrin aldehyde","Endosulfan sulfate","4.4`-DDT","Endrin ketone","Methoxychlor","Total Chlordane (sum)","Sum of DDD + DDE + DDT","Sum of Aldrin + Dieldrin","Dichlorvos","Demeton-S-methyl","Monocrotophos","Dimethoate","Diazinon","Chlorpyrifos-methyl","Parathion-methyl","Malathion","Fenthion",
-        "Chlorpyrifos","Parathion","Pirimphos-ethyl","Chlorfenvinphos","Bromophos-ethyl","Fenamiphos","Prothiofos","Ethion","Carbophenothion","Azinphos Methyl","Phenol","2-Chlorophenol","2-Methylphenol","3- & 4-Methylphenol","2-Nitrophenol","2.4-Dimethylphenol","2.4-Dichlorophenol","2.6-Dichlorophenol","4-Chloro-3-methylphenol","2.4.6-Trichlorophenol","2.4.5-Trichlorophenol",
-        "Pentachlorophenol","Naphthalene","Acenaphthylene","Acenaphthene","Fluorene","Phenanthrene","Anthracene","Fluoranthene","Pyrene","Benz(a)anthracene","Chrysene","Benzo(b+j)fluoranthene","Benzo(k)fluoranthene","Benzo(a)pyrene","Indeno(1.2.3.cd)pyrene","Dibenz(a.h)anthracene","Benzo(g.h.i)perylene","Sum of polycyclic aromatic hydrocarbons","Benzo(a)pyrene TEQ (zero)",
-        "C10 - C14 Fraction","C15 - C28 Fraction","C29 - C36 Fraction","C10 - C36 Fraction (sum)","C6 - C10 Fraction","C6 - C10 Fraction  minus BTEX (F1)",">C10 - C16 Fraction",">C16 - C34 Fraction",">C34 - C40 Fraction",">C10 - C40 Fraction (sum)",">C10 - C16 Fraction minus Naphthalene (F2)","Benzene","Toluene","Ethylbenzene","meta- & para-Xylene","ortho-Xylene","Total Xylenes",
-        "Sum of BTEX","BTEXNaphthalene","Dibromo-DDE","DEF","Phenol-d6","2-Chlorophenol-D4","2.4.6-Tribromophenol","2-Fluorobiphenyl","Anthracene-d10","4-Terphenyl-d14"
+    "Narangba.FixationTCLP": {
+        "Arsenic","Cadmium","Chromium","Copper","Lead","Nickel","Zinc","C10 - C14 Fraction","C15 - C28 Fraction","C29 - C36 Fraction","C10 - C36 Fraction (sum)",">C10 - C16 Fraction",">C16 - C34 Fraction",">C34 - C40 Fraction",">C10 - C40 Fraction (sum)",">C10 - C16 Fraction minus Naphthalene (F2)"
+        ,"C6 - C9 Fraction","C6 - C10 Fraction","C6 - C10 Fraction  minus BTEX (F1)","Benzene","Toluene","Ethylbenzene","meta- & para-Xylene","ortho-Xylene","Total Xylenes","Sum of BTEX","Naphthalene","1.2-Dichloroethane-D4","Toluene-D8","4-Bromofluorobenzene"    
     },
 
-    "Wacol.TradeWaste2025": {
-        "pH","Suspended Solids","Arsenic","Cadmium","Chromium","Copper","Lead","Nickel","Zinc","Mercury","Ammonia as N","Nitrite as N","Nitrate as N","Nitrite + Nitrate as N","Total Kjeldahl Nitrogen as N","Total Nitrogen as N","Total Phosphorus as P","Total Organic Carbon","Chemical Oxygen Demand","Biochemical Oxygen Demand","alpha-BHC","Hexachlorobenzene (HCB)","beta-BHC","gamma-BHC - (Lindane)",
-        "delta-BHC","Heptachlor","Aldrin","Heptachlor epoxide","trans-Chlordane","alpha-Endosulfan","cis-Chlordane","Dieldrin","4.4`-DDE","Endrin","beta-Endosulfan","4.4`-DDD","Endrin aldehyde","Endosulfan sulfate","4.4`-DDT","Endrin ketone","Methoxychlor","Total Chlordane (sum)","Sum of DDD + DDE + DDT","Sum of Aldrin + Dieldrin","Dichlorvos","Demeton-S-methyl","Monocrotophos","Dimethoate","Diazinon",
-        "Chlorpyrifos-methyl","Parathion-methyl","Malathion","Fenthion","Chlorpyrifos","Parathion","Pirimphos-ethyl","Chlorfenvinphos","Bromophos-ethyl","Fenamiphos","Prothiofos","Ethion","Carbophenothion","Azinphos Methyl","C6 - C9 Fraction","C10 - C14 Fraction","C15 - C28 Fraction","C29 - C36 Fraction","C10 - C36 Fraction (sum)","C6 - C10 Fraction","C6 - C10 Fraction  minus BTEX (F1)",">C10 - C16 Fraction",
-        ">C16 - C34 Fraction",">C34 - C40 Fraction",">C10 - C40 Fraction (sum)",">C10 - C16 Fraction minus Naphthalene (F2)","Benzene","Toluene","Ethylbenzene","meta- & para-Xylene","ortho-Xylene","Total Xylenes","Sum of BTEX","Naphthalene","4-Chlorophenoxy acetic acid","2.4-DB","Dicamba","Mecoprop","MCPA","2.4-DP","2.4-D","Triclopyr","Silvex (2.4.5-TP/Fenoprop)","2.4.5-T","MCPB","Picloram","Clopyralid","Fluroxypyr",
-        "2.6-D","2.4.6-T","Glyphosate","AMPA","Perfluoropropane sulfonic acid (PFPrS)","Perfluorobutane sulfonic acid (PFBS)","Perfluoropentane sulfonic acid (PFPeS)","Perfluorohexane sulfonic acid (PFHxS)","Perfluoroheptane sulfonic acid (PFHpS)","Perfluorooctane sulfonic acid (PFOS)","Perfluorononane sulfonic acid (PFNS)","Perfluorodecane sulfonic acid (PFDS)","Perfluorobutanoic acid (PFBA)","Perfluoropentanoic acid (PFPeA)",
-        "Perfluorohexanoic acid (PFHxA)","Perfluoroheptanoic acid (PFHpA)","Perfluorooctanoic acid (PFOA)","Perfluorononanoic acid (PFNA)","Perfluorodecanoic acid (PFDA)","Perfluoroundecanoic acid (PFUnDA)","Perfluorododecanoic acid (PFDoDA)","Perfluorotridecanoic acid (PFTrDA)","Perfluorotetradecanoic acid (PFTeDA)","Perfluorohexadecanoic acid (PFHxDA)","Perfluorooctane sulfonamide (FOSA)","N-Methyl perfluorooctane sulfonamide (MeFOSA)",
-        "N-Ethyl perfluorooctane sulfonamide (EtFOSA)","N-Methyl perfluorooctane sulfonamidoethanol (MeFOSE)","N-Ethyl perfluorooctane sulfonamidoethanol (EtFOSE)","N-Methyl perfluorooctane sulfonamidoacetic acid (MeFOSAA)","N-Ethyl perfluorooctane sulfonamidoacetic acid (EtFOSAA)","4:2 Fluorotelomer sulfonic acid (4:2 FTS)","6:2 Fluorotelomer sulfonic acid (6:2 FTS)","8:2 Fluorotelomer sulfonic acid (8:2 FTS)","10:2 Fluorotelomer sulfonic acid (10:2 FTS)",
-        "Sum of PFAS","Sum of PFHxS and PFOS","Sum of PFAS (WA DER List)","Dibromo-DDE","DEF","1.2-Dichloroethane-D4","Toluene-D8","4-Bromofluorobenzene","2.4-Dichlorophenyl Acetic Acid","13C4-PFOS","13C8-PFOA"
-   },
+    "Narangba.TradeWaste": {
+        "pH Value","Dissolved Solids","Suspended Solids","Sulfate as SO4 - Turbidimetric","Arsenic","Cadmium","Chromium","Copper","Lead","Nickel","Zinc","Iron","Mercury","Nitrite + Nitrate as N","Total Kjeldahl Nitrogen as N","Total Nitrogen as N","Total Phosphorus as P","Total Organic Carbon"
+        ,"Chemical Oxygen Demand","Biochemical Oxygen Demand","Oil & Grease","C6 - C9 Fraction","C10 - C14 Fraction","C15 - C28 Fraction","C29 - C36 Fraction","C10 - C36 Fraction (sum)","C6 - C10 Fraction","C6 - C10 Fraction  minus BTEX (F1)",">C10 - C16 Fraction",">C16 - C34 Fraction",">C34 - C40 Fraction"
+        ,">C10 - C40 Fraction (sum)",">C10 - C16 Fraction minus Naphthalene (F2)","Benzene","Toluene","Ethylbenzene","meta- & para-Xylene","ortho-Xylene","Total Xylenes","Sum of BTEX","Naphthalene","1.2-Dichloroethane-D4","Toluene-D8","4-Bromofluorobenzene"   
+    },
 
     "Wacol.TradeWasteExt": {
         "alpha-BHC","Hexachlorobenzene (HCB)","beta-BHC","gamma-BHC - (Lindane)","delta-BHC","Heptachlor","Aldrin","Heptachlor epoxide","trans-Chlordane","alpha-Endosulfan","cis-Chlordane","Dieldrin","4.4`-DDE","Endrin","beta-Endosulfan","4.4`-DDD","Endrin aldehyde","Endosulfan sulfate","4.4`-DDT","Endrin ketone","Methoxychlor","Total Chlordane (sum)","Sum of DDD + DDE + DDT","Sum of Aldrin + Dieldrin",
@@ -69,10 +56,10 @@ ID_COLUMNS = ["File Name","Sample Date"]
 
 # Non-analyte identifier columns you always want back
 TABLE_ID_COLUMNS: Dict[str, List[str]] = {
-    "Wacol.Fixation2025FullScan": ["File","Sample Date"],
-    "Wacol.Fixation2025TCLP": ["File","Sample Date"],
-    "Wacol.TradeWaste2025": ["File Name","Sample Date"],
-    "Wacol.TradeWasteExt": ["File Name","Sample Date"]
+    "Narangba.Fixation": ["Sample Date"],
+    "Narangba.FixationTCLP":["Sample Date"],
+    "Narangba.TradeWaste": ["Sample Date"],
+    "Wacol.TradeWasteExt": ["Sample Date"]
 }
 
 # ========= DB CONNECT =========
@@ -110,8 +97,8 @@ def connect_with_fallback(timeout_seconds: int = 60) -> pyodbc.Connection:
 def normalize_payload(data) -> Dict[str, List[str]]:
     """
     Support two payload shapes:
-      A) { "selections": { "external": ["arsenic", ...], "pfastopa": ["pfos"] }, "startDate": "...", "endDate": "..." }
-      B) { "selections": [ { "table": "Jackson.DSExt", "analyte":"arsenic" }, ... ], ... }
+      A) { "selections": { "external": ["arsenic", ...", "pfastopa": ["pfos"] }, "startDate": "...", "endDate": "..." }
+      B) { "selections": [ { "table": "Jackson.DSExt", "analyte":"arsenic" }, ... ", ... }
     Returns a dict { group_key_or_table: [analytes...] } keyed by group-id (preferred) if possible.
     """
     sel = data.get("selections", [])
